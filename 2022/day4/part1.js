1ea4bbb107925877208e92258d2dfc179673ff49
Huu Nguyen <nguyenhuuit@yahoo.com>
2022-12-13 21:23:54 +0700

    Add day 4

---
+++ 2022/day4/part1.js
@@ -0,0 +1,9 @@
+const solution = input => {
+  return input
+    .split(/\n/)
+    .map(p => p.split(/[-,]/).map(int))
+    .filter(p => (p[0] <= p[2] && p[1] >= p[3]) || (p[2] <= p[0] && p[3] >= p[1]))
+    .length
+}
+
+module.exports = solution;
