473f311c0f05a5a84ec734f623109b2e7ea5d1f7
Gia Vinh <nguyenhuu.uit.donald@gmail.com>
2023-07-27 20:08:10 +0700

    Add spinners

--- 2015/day3/part2.py
+++ 2015/day3/part2.py
@@ -1,10 +1,8 @@
 def solution(input):
-  s = set()
   delta = {">":(1,0),"<":(-1,0),"^":(0,1),"v":(0,-1)}
   xs,ys = 0,0
   xr,yr = 0,0
-  s.add((xs,ys))
-  s.add((xr,yr))
+  s = {(0,0)}
   for i in range(len(input)):
     thay_doi = delta[input[i]]
     if i % 2 == 1:
