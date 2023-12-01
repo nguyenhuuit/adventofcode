6fbce30ac496f2a0e9856da5a199474388121d1e
Huu Nguyen <nguyenhuuit@yahoo.com>
2023-12-01 14:49:52 +0700

    Update help message

--- src/components/constants.ts
+++ src/components/constants.ts
@@ -4,7 +4,7 @@ const bg = (s: string): string => chalk.bold(chalk.greenBright(s))
 const by = (s: string): string => chalk.bold(chalk.yellowBright(s))
 
 export const HELP_MESSAGE = `Instructions:
-${by('1')}: Run ${bg('part1')} solution       ${by('Enter')}: Re-run solution
-${by('2')}: Run ${bg('part2')} solution         ${by('X')}  : Terminate solution
-${by('S')}: Run solution with ${bg('sample')}   ${by('H')}  : Print the instructions
-${by('I')}: Run solution with ${bg('input')}    ${by('Q')}  : Quit application`
+${by('0-9')}: Run ${bg('partX')} solution         ${by('Enter')}: Re-run solution
+${by('U')}  : Submit solution            ${by('X')}    : Terminate solution
+${by('S')}  : Run solution with ${bg('sample')}   ${by('H')}    : Print the instructions
+${by('I')}  : Run solution with ${bg('input')}    ${by('Q')}    : Quit application`
