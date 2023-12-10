1e955171b226eaec654d3dacf53e5a107249e192
Gia Vinh <nguyenhuu.uit.donald@gmail.com>
2023-12-10 15:22:34 +0700

    Add day 9 year 2023

---
+++ 2023/day9/part1.py
@@ -0,0 +1,28 @@
+def subtract(a):
+  k = []
+  b = True
+  for i in range(1,len(a)):
+    k.append(a[i]-a[i-1])
+    if a[i] - a[i-1] != 0:
+      b = False
+  return (k,b)
+      
+def predict(a):
+  l = [a]
+  while True:
+    s,b = subtract(l[-1])
+    l.append(s)
+    if b:
+      break
+  n = 0
+  for i in range(len(l)-2,-1,-1):
+    n = n + l[i][-1]
+  return n
+
+def solution(input):
+  d = input.split("\n")
+  rs = 0
+  for i in range(len(d)):
+    rs += predict(list(map(int, d[i].split())))
+  return rs
+  
